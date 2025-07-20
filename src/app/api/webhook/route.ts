154222c727a9229772e4d30ac6c3e6f0711fb4bd
tomdekan <t@tomdekan.com>
2025-07-20 15:24:29 +0100

    Add

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -23,12 +23,16 @@ export async function POST(req: NextRequest) {
 	const body = await req.text();
 	const sig = req.headers.get("stripe-signature") as string;
 
+	if (!process.env.STRIPE_WEBHOOK_SECRET) {
+		throw new Error("STRIPE_WEBHOOK_SECRET is not set");
+	}
+
 	let event: Stripe.Event;
 	try {
 		event = stripe.webhooks.constructEvent(
 			body,
 			sig,
-			process.env.STRIPE_WEBHOOK_SECRET!,
+			process.env.STRIPE_WEBHOOK_SECRET,
 		);
 	} catch (err: unknown) {
 		console.error(
